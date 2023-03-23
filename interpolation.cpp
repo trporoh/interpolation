10b1471b18c2b3091d469716e41d19a6b5f91e07
Ilya <porokhov.ilya@yandex.ru>
2023-03-23 19:50:01 +0300

    add lagrange interpolation

--- interpolation.cpp
+++ interpolation.cpp
@@ -3,10 +3,12 @@
 
 using namespace std;
 
-int inputXnY(int n, vector<pair<int,int>> XnY) {
+vector<pair<double,double>> inputXnY(int n, vector<pair<double,double>> XnY) {
 
 	int x = 0, y = 0;
-	vector<pair<int, int>> input;
+	vector<pair<double, double>> input;
+
+	cout << "inpit data" << endl << "|x  y|" << endl;
 
 	for (int i = 0; i < n; i++) {
 		cin >> x >> y;
@@ -15,16 +17,43 @@ int inputXnY(int n, vector<pair<int,int>> XnY) {
 
 	XnY = input;
 
-	system("cls");
+	system("clear");
 
-	return 0;
+	return XnY;
+}
+
+double Lagrange(vector<pair<double,double>> XnY, double x) {
+
+	double P;
+	double resoult = 0;
+	for (long unsigned int i = 0; i <= XnY.size(); i++) {
+		P = 1;
+		for (long unsigned int j = 0; j <= XnY.size(); j++) {
+			if (i != j) {
+				P *= (x - XnY[j].first) / (XnY[i].first - XnY[j].first);
+			}
+		}
+		resoult += XnY[i].second * P;
+	}
+
+	return resoult;
 }
 
 int main() {
-	int n = 4;
-	vector<pair<int, int>> XnY;
 
-	inputXnY(n, XnY);
+	int n;
+	double Lagr;
+	vector<pair<double, double>> XnY;
+
+	cout << "enter the number of points: ";
+	cin >> n;
+
+	system("clear");
+
+	XnY = inputXnY(n, XnY);
+	Lagr = Lagrange(XnY, 2.5);
+	
+	cout << "| " << Lagr << " | " << endl;
 
 	return 0;
 }
