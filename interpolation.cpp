9a41fb5f99b457aa5c33efc7a0b4c1b878b65bcc
Ilya <porokhov.ilya@yandex.ru>
2023-03-24 13:00:07 +0300

    add Eitkin scheme

--- interpolation.cpp
+++ interpolation.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<pair<double,double>> inputXnY(int n, vector<pair<double,double>> XnY) {
 
-	int x = 0, y = 0;
+	double x = 0, y = 0;
 	vector<pair<double, double>> input;
 
 	cout << "inpit data" << endl << "|x  y|" << endl;
@@ -39,10 +39,41 @@ double Lagrange(vector<pair<double,double>> XnY, double x) {
 	return resoult;
 }
 
+double Eitkin(vector<pair<double,double>> XnY, double x) {
+
+	vector<vector<double>> scheme(XnY.size());
+	double P;
+	
+	for (long unsigned int i = 0; i <= XnY.size() - 1; i++) {
+		P = 1;
+		for (long unsigned int j = 0; j <= XnY.size() -1 ; j++) {
+			if (i != j) {
+				P *= (x - XnY[j].first) / (XnY[i].first - XnY[j].first);
+			}
+		}
+		scheme[i].push_back(XnY[i].second * P);
+		cout << scheme[i].at(0) << "   ";
+	}
+	cout << endl;
+		
+	for (int j = 1; j < (int)XnY.size(); j++) {
+
+		for (int i = 0; i < (int)XnY.size() - j; i++) {
+
+			scheme.at(i).push_back((scheme.at(i).at(j - 1) * (x - XnY.at(i + j).first) - scheme.at(i + 1).at(j - 1) * (x - XnY.at(i).first)) / (XnY.at(i).first - XnY.at(i + j).first));
+			cout << scheme.at(i).at(j) << "   ";
+		}
+		cout << endl;
+	}
+	
+	return scheme.at(0).at(XnY.size() - 1);
+}
+
 int main() {
 
-	int n;
-	double Lagr;
+	int n, var;
+	double Lagr, Eitk;
+
 	vector<pair<double, double>> XnY;
 
 	cout << "enter the number of points: ";
@@ -51,9 +82,20 @@ int main() {
 	system("clear");
 
 	XnY = inputXnY(n, XnY);
-	Lagr = Lagrange(XnY, 2.5);
-	
-	cout << "| " << Lagr << " | " << endl;
+
+	cout << "press 1 for Lagr, 2 for Eitken: " << endl;
+	cin >> var;
+
+	switch (var) {
+	case 1:
+		Lagr = Lagrange(XnY,2);
+		cout << "| " << Lagr << " | ";
+		break;
+	case 2:
+		Eitk = Eitkin(XnY, 2);
+		cout << "| " << Eitk << " | ";
+		break;
+	}
 
 	return 0;
 }
